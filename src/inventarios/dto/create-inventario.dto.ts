875a0e2b400e326f81266bac24616425e2fdb344
Alvaro Ch <chambia04@gmail.com>
2024-11-19 13:24:10 -0400

    actualizacion de inventario

--- src/inventarios/dto/create-inventario.dto.ts
+++ src/inventarios/dto/create-inventario.dto.ts
@@ -1,46 +1,77 @@
 import { IsString, IsDateString, IsOptional, Length } from 'class-validator';
 
 export class CreateInventarioDto {
-  @IsString()
+  /*@IsString()
   @Length(1, 20)
-  codigo: string;
+  iDLamina: string;
 
   @IsString()
   @Length(1, 50)
-  raza: string;
+  tipoLamina: string;
+
+  @IsString()
+  @Length(1, 50)
+  dimensionesLamina: string;
+
+  @IsString()
+  @Length(1, 50)
+  cantidadDisponible: string;
+
+  @IsString()
+  @Length(1, 10)
+  espesor: string;
 
   @IsString()
   @Length(1, 50)
   color: string;
 
-  @IsString()
-  @Length(1, 50)
-  marcaCarimbo: string;
-
-  @IsString()
-  @Length(1, 10)
-  sexo: string;
+  @IsDateString()
+  fechaIngreso: string;
 
   @IsString()
   @Length(1, 50)
-  categoria: string;
+  estado: string;
+
+  //@IsString()
+  //@Length(1, 10)
+  //enInventario: string;
+
+  //@IsString()
+  //@Length(1, 100)
+  //unidad: string;
+
+  //@IsString()
+  //@Length(1, 20)
+  //nroArete: string;*/
+  @IsString()
+  @Length(1, 50)
+  iDLamina: string;
+
+  @IsString()
+  @Length(1, 50)
+  tipoLamina: string;
+
+  @IsString()
+  @Length(1, 50)
+  dimensionesLamina: string;
+
+  @IsString()
+  @Length(1, 50)
+  cantidadDisponible: string;
+
+  @IsString()
+  @Length(1, 50)
+  espesor: string;
+
+  @IsString()
+  @Length(1, 50)
+  color: string;
 
   @IsDateString()
-  fechaNac: string;
+  @Length(1, 50)
+  fechaIngreso: string;
 
   @IsString()
   @Length(1, 50)
-  tipoGanado: string;
-
-  @IsString()
-  @Length(1, 10)
-  enInventario: string;
-
-  @IsString()
-  @Length(1, 100)
-  unidad: string;
-
-  @IsString()
-  @Length(1, 20)
-  nroArete: string;
+  estado: string;
 }
--- src/inventarios/entities/inventario.entity.ts
+++ src/inventarios/entities/inventario.entity.ts
@@ -9,7 +9,7 @@ export class Inventario {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 20 })
+  /*@Column({ type: 'varchar', length: 20 })
   codigo: string;
 
   @Column({ type: 'varchar', length: 50 })
@@ -40,7 +40,31 @@ export class Inventario {
   unidad: string;
 
   @Column({ type: 'varchar', length: 20 })
-  nroArete: string;
+  nroArete: string;*/
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  iDLamina: string; // ID de la lámina
+
+  @Column({ type: 'varchar', length: 100 })
+  tipoLamina: string; // Tipo de Lámina
+
+  @Column({ type: 'varchar', length: 50 })
+  dimensionesLamina: string; // Dimensiones de la Lámina (Ancho x Alto)
+
+  @Column({ type: 'varchar' })
+  cantidadDisponible: string; // Cantidad Disponible
+
+  @Column({ type: 'varchar' })
+  espesor: string; // Espesor de la Lámina
+
+  @Column({ type: 'varchar', length: 50 })
+  color: string; // Color
+
+  @Column({ type: 'varchar' })
+  fechaIngreso: string; // Fecha de Ingreso al Inventario
+
+  @Column({ type: 'varchar', length: 20 })
+  estado: string; // Estado (Disponible/Agotado)
 
   @CreateDateColumn()
   createdAt: Date;
